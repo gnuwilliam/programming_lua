1df554f05b2f10e6e8ddc3833cd2225509dcd97e
William Oliveira <sudowilliam@gmail.com>
2014-05-18 02:26:33 -0300

    Fix negative value problem

--- factorial.lua
+++ factorial.lua
@@ -3,6 +3,8 @@
 function factorial (n)
     if n == 0 then
         return 1
+    elseif n < 0 then
+        return print("Please enter a valid number!")
     else
         return n * factorial(n-1)
     end
